c8c90885b45e218ac1719d87d8f5076e5ef93970
Hanyi Wang <53130378+Hanyi-Wang-DarkStardust@users.noreply.github.com>
2022-07-23 13:00:01 +0800

    ci: change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,154 +1,168 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
     'airbnb',
-    'airbnb/hooks',
+    'prettier',
+    'plugin:compat/recommended',
+    'plugin:jest/recommended',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:markdown/recommended',
   ],
-  root: true,
   env: {
-    node: true,
-    jest: true,
     browser: true,
+    node: true,
+    jasmine: true,
+    jest: true,
+    es6: true,
   },
-  plugins: [
-    '@typescript-eslint',
-    '@typescript-eslint/eslint-plugin',
-    'react-hooks',
-    'unicorn',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.json'],
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
+  settings: {
+    react: {
+      version: 'detect',
     },
+    polyfills: ['Promise', 'URL'],
   },
-  globals: {
-    React: true,
-    shallow: true,
-    JSX: true,
-    expect: true
-  },
-  rules: {
-    curly: ['error', 'multi-line'],
-    'guard-for-in': 'error',
-    'no-alert': 'error',
-    'no-console': ["warn", { allow: ["warn", "error"] }],
-    'no-labels': ['error', { allowLoop: false, allowSwitch: false }],
-    'no-lone-blocks': 'error',
-    'no-script-url': 'error',
-    'no-self-compare': 'error',
-    'no-shadow': 'error',
-    '@typescript-eslint/no-use-before-define': ['error', { functions: true, classes: true, variables: true }],
-    'default-param-last': 'off',
-    '@typescript-eslint/default-param-last': 'error',
-    'object-curly-newline': ['error', {
-      ObjectExpression: { minProperties: 4, multiline: true, consistent: true },
-      ObjectPattern: { minProperties: 4, multiline: true, consistent: true },
-      ImportDeclaration: { minProperties: 4, multiline: true, consistent: true },
-      ExportDeclaration: { minProperties: 4, multiline: true, consistent: true },
-    }],
-    'space-unary-ops': ['error', {
-      words: true,
-      nonwords: false,
-      overrides: {
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'babel', 'jest', '@typescript-eslint', 'react-hooks', 'unicorn', 'markdown'],
+  // https://github.com/typescript-eslint/typescript-eslint/issues/46#issuecomment-470486034
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': 2,
+        '@typescript-eslint/consistent-type-imports': 2,
       },
-    }],
-    'jsx-quotes': ['error', 'prefer-double'],
-    'linebreak-style': ['error', 'unix'],
-    'unicorn/filename-case': ['error', {
-      case: 'kebabCase',
-    }],
-    'spaced-comment': ['error', 'always', {
-      line: {
-        markers: ['#region', '#endregion', 'region', 'endregion'],
-      },
-    }],
-
-    'no-fallthrough': 'off',
-    'no-implied-eval': 'error',
-    complexity: ['warn', 10],
-    'new-cap': ['off'],
-    'linebreak-style': 0,
-    'react/jsx-uses-vars': 'error',
-    '@typescript-eslint/restrict-plus-operands': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    '@typescript-eslint/prefer-regexp-exec': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/consistent-type-assertions': 'off',
-
-    'no-unused-vars': 'error',
-    '@typescript-eslint/no-unused-vars': [
-      'error', {
-        args: 'after-used',
-        argsIgnorePattern: '^_.+',
-      },
-    ],
-    'dot-notation': 'off',
-    '@typescript-eslint/dot-notation': ['warn', { allowKeywords: true }],
-    'func-call-spacing': 'off',
-    '@typescript-eslint/func-call-spacing': ['error', 'never'],
-    'brace-style': 'off',
-    '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-    'comma-spacing': ['error', { before: false, after: true }],
-    '@typescript-eslint/comma-spacing': 'off',
-    'no-array-constructor': 'off',
-    '@typescript-eslint/no-array-constructor': 'error',
-    'no-dupe-class-members': 'off',
-    '@typescript-eslint/no-dupe-class-members': 'error',
-    'no-extra-semi': 'off',
-    '@typescript-eslint/no-extra-semi': 'error',
-    'no-useless-constructor': 'off',
-    '@typescript-eslint/no-useless-constructor': 'warn',
-    quotes: 'off',
-    '@typescript-eslint/quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
-    '@typescript-eslint/require-await': 'off',
-    semi: 'off',
-    '@typescript-eslint/semi': ['error', 'always'],
-    'space-before-function-paren': 'off',
-    '@typescript-eslint/space-before-function-paren': ['error', {
-      anonymous: 'always',
-      named: 'never',
-      asyncArrow: 'always',
-    }],
-    '@typescript-eslint/interface-name-prefix': 'off',
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": "error",
-    'import/order': [
-      'warn',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-        ],
-        'newlines-between': 'never',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
+    },
+    {
+      // In v2, explicitly apply eslint-plugin-markdown's `markdown`
+      // processor on any Markdown files you want to lint.
+      files: ['components/*/demo/*.md'],
+      processor: 'markdown/markdown',
+    },
+    {
+      // In v2, configuration for fenced code blocks is separate from the
+      // containing Markdown file. Each code block has a virtual filename
+      // appended to the Markdown file's path.
+      files: [
+        'components/*/demo/*.md/*.ts',
+        'components/*/demo/*.md/*.tsx',
+        'components/*/demo/*.md/*.js',
+        'components/*/demo/*.md/*.jsx',
+      ],
+      // Configuration for fenced code blocks goes with the override for
+      // the code block's virtual filename, for example:
+      parserOptions: {
+        ecmaFeatures: {
+          impliedStrict: true,
         },
       },
-    ],
-    'sort-imports': [
-      'warn',
+      globals: {
+        React: true,
+        ReactDOM: true,
+        mountNode: true,
+      },
+      rules: {
+        indent: 0,
+        'default-case': 0,
+        'eol-last': 0,
+        'no-console': 0,
+        'no-plusplus': 0,
+        'no-script-url': 0,
+        'prefer-rest-params': 0,
+        'compat/compat': 0,
+        'class-methods-use-this': 0,
+        'react/no-access-state-in-setstate': 0,
+        'react/destructuring-assignment': 0,
+        'react/no-multi-comp': 0,
+        'react/no-array-index-key': 0,
+        'jsx-a11y/href-no-hash': 0,
+        'jsx-a11y/control-has-associated-label': 0,
+        'import/no-extraneous-dependencies': 0,
+        'react/jsx-no-constructed-context-values': 0,
+        'react/no-unstable-nested-components': 0,
+      },
+    },
+  ],
+  rules: {
+    'react/jsx-one-expression-per-line': 0,
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-indent': 0,
+    'react/jsx-wrap-multilines': ['error', { declaration: false, assignment: false }],
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/destructuring-assignment': 0, // TODO: remove later
+    'react/require-default-props': 0,
+    'react/sort-comp': 0,
+    'react/display-name': 0,
+    'react/static-property-placement': 0,
+    'react/jsx-no-bind': 0, // Should not check test file
+    'react/no-find-dom-node': 0,
+    'react/no-unused-prop-types': 0,
+    'react/default-props-match-prop-types': 0,
+    'react-hooks/rules-of-hooks': 2, // Checks rules of Hooks
+    'react/function-component-definition': 0,
+    'react/no-unused-class-component-methods': 0,
+    'import/extensions': 0,
+    'import/no-cycle': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
       {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
+        devDependencies: [
+          'site/**',
+          'tests/**',
+          'scripts/**',
+          '**/*.test.js',
+          '**/__tests__/*',
+          '*.config.js',
+          '**/*.md',
+        ],
       },
     ],
-  }
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/anchor-has-content': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    // label-has-for has been deprecated
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
+    'jsx-a11y/label-has-for': 0,
+
+    'comma-dangle': ['error', 'always-multiline'],
+    'consistent-return': 0, // TODO: remove later
+    'no-param-reassign': 0, // TODO: remove later
+    'no-underscore-dangle': 0,
+    // for (let i = 0; i < len; i++)
+    'no-plusplus': 0,
+    // https://eslint.org/docs/rules/no-continue
+    // labeledLoop is conflicted with `eslint . --fix`
+    'no-continue': 0,
+    // ban this for Number.isNaN needs polyfill
+    'no-restricted-globals': 0,
+    'max-classes-per-file': 0,
+
+    'jest/no-test-callback': 0,
+    'jest/expect-expect': 0,
+    'jest/no-done-callback': 0,
+    'jest/valid-title': 0,
+    'jest/no-conditional-expect': 0,
+
+    'unicorn/better-regex': 2,
+    'unicorn/prefer-string-trim-start-end': 2,
+    'unicorn/expiring-todo-comments': 2,
+    'unicorn/no-abusive-eslint-disable': 2,
+
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2540#issuecomment-692866111
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 2,
+    'no-shadow': 0,
+    '@typescript-eslint/no-shadow': [2, { ignoreTypeValueShadow: true }],
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2528#issuecomment-689369395
+    'no-undef': 0,
+  },
+  globals: {
+    gtag: true,
+  },
 };
